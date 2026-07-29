6c164e621f4c4370d66681bdba0a368409f065d9
ivanssantana <ivansantospriv@gmail.com>
2026-07-29 19:32:22 -0300

    Corrigindo bug do autocompleter

--- src/utils.js
+++ src/utils.js
@@ -56,12 +56,12 @@ export function atualizarTextArea(tokens) {
 
 export function atualizarCodigoRenderizado(codigoStr, lexer) {
     let tokens = lexer.tokenizer(codigoStr);
-    let tokensPosErros = lexer.detectadorErros(tokens); 
+    tokens = lexer.detectadorErros(tokens); 
 
-    if (JSON.stringify(tokensPosErros) === JSON.stringify(tokens)) {
-        tokensPosErros = autoCompleteTag(tokens);
-    };
+    if (!tokens.some(token => token.tipo === tiposTokens.ERRO)) {
+        tokens = autoCompleteTag(tokens);
+    }; 
 
-    atualizarTextArea(tokensPosErros);
-    lexer.colorizer(tokensPosErros);
+    atualizarTextArea(tokens);
+    lexer.colorizer(tokens);
 }
